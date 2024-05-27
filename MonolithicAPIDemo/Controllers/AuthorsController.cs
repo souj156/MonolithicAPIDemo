280465d2a4196dcedaab354fba3063793f1d8a7d
Siva Arigela <siva.arigela@colruytgroup.com>
2024-05-27 14:20:47 +0200

    Add project files.

---
+++ MonolithicAPIDemo/Controllers/AuthorsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Monolith.Data;
+using Monolith.Models;
+
+namespace MonolithicAPIDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ILogger<AuthorsController> _logger;
+        private readonly Repository _repository;
+
+        public AuthorsController(ILogger<AuthorsController> logger, Repository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Author> Get() => _repository.GetAuthors();
+    }
+}
---
+++ MonolithicAPIDemo/Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Monolith.Data;
+using Monolith.Models;
+
+namespace MonolithicAPIDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+        private readonly Repository _repository;
+
+        public BooksController(ILogger<BooksController> logger, Repository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get() => _repository.GetBooks();
+    }
+}
